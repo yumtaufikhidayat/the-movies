7cb00275ab1a9264392685886fa62399934d1e6a
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-06-23 21:58:40 +0700

    Init project.

---
+++ app/src/main/java/com/yumtaufikhidayat/themovies/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.yumtaufikhidayat.themovies.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yumtaufikhidayat.themovies.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
