e4af47f4d0515d5d1cc5154106b8205fa8e2c2ec
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-06-24 20:13:14 +0700

    Add navigation controller to main activity.

--- app/src/main/java/com/yumtaufikhidayat/themovies/ui/MainActivity.kt
+++ app/src/main/java/com/yumtaufikhidayat/themovies/ui/MainActivity.kt
@@ -2,14 +2,28 @@ package com.yumtaufikhidayat.themovies.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.yumtaufikhidayat.themovies.R
 import com.yumtaufikhidayat.themovies.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var navController: NavController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setNavHost()
+    }
+
+    private fun setNavHost() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController = navHostFragment.findNavController()
     }
 }
